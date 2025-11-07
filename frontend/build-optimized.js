b17deeb63d70df55d86405b3f372fed5beb4604e
Chrisagon <christophe.thomas@fesc.asso.fr>
2025-11-07 10:47:10 +0100

    new file:   frontend/build-optimized.js modified:   frontend/package.json modified:   render.yaml

---
+++ frontend/build-optimized.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+/**
+ * Optimized build script that disables fork-ts-checker
+ * to prevent OOM errors during production builds
+ */
+
+process.env.CI = 'true';
+process.env.GENERATE_SOURCEMAP = 'false';
+process.env.SKIP_PREFLIGHT_CHECK = 'true';
+process.env.TSC_COMPILE_ON_ERROR = 'true';
+process.env.ESLINT_NO_DEV_ERRORS = 'true';
+
+// Disable fork-ts-checker by setting environment variable
+process.env.FORK_TS_CHECKER_ENABLED = 'false';
+
+// Increase Node heap size
+if (!process.env.NODE_OPTIONS) {
+  process.env.NODE_OPTIONS = '--max-old-space-size=8192';
+}
+
+// Import and run react-scripts build
+require('react-scripts/scripts/build');
