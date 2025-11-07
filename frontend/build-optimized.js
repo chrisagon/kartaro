fa3b174ababc286c889dbe55dd73f4e1d6e11080
Chrisagon <christophe.thomas@fesc.asso.fr>
2025-11-07 11:12:17 +0100

    modified:   frontend/build-optimized.js

--- frontend/build-optimized.js
+++ frontend/build-optimized.js
@@ -5,13 +5,13 @@
  * to prevent OOM errors during production builds
  */
 
+// Set all optimization variables
 process.env.CI = 'true';
 process.env.GENERATE_SOURCEMAP = 'false';
 process.env.SKIP_PREFLIGHT_CHECK = 'true';
 process.env.TSC_COMPILE_ON_ERROR = 'true';
 process.env.ESLINT_NO_DEV_ERRORS = 'true';
-
-// Disable fork-ts-checker by setting environment variable
+process.env.DISABLE_NEW_JSX_TRANSFORM = 'true';
 process.env.FORK_TS_CHECKER_ENABLED = 'false';
 
 // Increase Node heap size
